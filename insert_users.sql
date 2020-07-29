b5b38df2a44538ea0d47a1f487e283f4db6bf0d3
Murad Nurmukhamedov <mns130691@gmail.com>
2020-07-29 18:04:54 +0300

    create insert_users and insert_orders files which added data into the tables

---
+++ insert_users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (first_name, last_name, email) 
+VALUES ('Chandler', 'Bing', 'bing@gmail.com'),
+       ('Joey', 'Tribbiani', 'how_you_oing@gmail.com'),
+       ('Ross', 'Geller', 'ross@gmail.com'),
+       ('Rachel', 'Green', 'rach@gmail.com'),
+       ('Monica', 'Geller', 'mon@aol.com'),
+       ('Phoebe', 'Buffay', 'phoebs@gmail.com');
